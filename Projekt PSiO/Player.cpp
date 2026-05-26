24cc0c92831c2141701ba0d2b66d8bbbd6ee021b
adrian10012 <adrianwichowski@wp.pl>
2026-05-26 23:42:13 +0200

    ruch-gracza poprawka

--- Projekt PSiO/Player.cpp
+++ Projekt PSiO/Player.cpp
@@ -2,21 +2,17 @@
 
 Player::Player() {
     shape.setRadius(16.f);
-    shape.setOrigin(16.f, 16.f); // Ustawiamy środek kółka w jego centrum
+    shape.setOrigin(16.f, 16.f);
     shape.setFillColor(sf::Color::Green);
-
-    pos = sf::Vector2f(400.f, 300.f); // Pozycja startowa (środek ekranu 800x600)
-    speed = 250.f; // Szybkość poruszania się
+    pos = sf::Vector2f(400.f, 300.f);
+    speed = 250.f;
 }
 
 void Player::update(float dt) {
-    // Poruszanie klawiszami WSAD
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) pos.y -= speed * dt;
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) pos.y += speed * dt;
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) pos.x -= speed * dt;
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) pos.x += speed * dt;
-
-    // Aktualizacja pozycji kształtu
     shape.setPosition(pos);
 }
 
